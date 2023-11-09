a1d5447ad63882d0034e01280b7d878b80cb230b
gustawx <haslO1234?>
2023-11-09 22:23:34 +0100

    feat(meal_details.dart, meal_item.dart): add Hero widget for image transition feat(meal_details.dart): replace static star icon with AnimatedSwitcher for smoother transition fix(meal_details.dart, meal_item.dart): wrap Image.network and FadeInImage in Hero widget for smooth image transition between screens

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -33,17 +33,32 @@ class MealDetailsScreen extends ConsumerWidget {
                 ),
               );
             },
-            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 500),
+              child: Icon(
+                isFavorite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavorite),
+              ),
+              transitionBuilder: (child, animation) {
+                return RotationTransition(
+                  turns: Tween(begin: 0.8, end: 1.0).animate(animation),
+                  child: child,
+                );
+              },
+            ),
           )
         ]),
         body: SingleChildScrollView(
           child: Column(
             children: [
-              Image.network(
-                meal.imageUrl,
-                height: 300,
-                width: double.infinity,
-                fit: BoxFit.cover,
+              Hero(
+                tag: meal.id,
+                child: Image.network(
+                  meal.imageUrl,
+                  height: 300,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
               ),
               const SizedBox(height: 14),
               Text(
